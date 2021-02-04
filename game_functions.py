d84891dbcd78ac951b9210589034e880c54ea531
skywkcn <skywkcn@outlook.com>
2021-02-04 17:54:30 +0800

    更新

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单机Play按钮时开始游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
+        
         # 隐藏光标
         pygame.mouse.set_visible(False)
         # 重置游戏统计信息
@@ -103,8 +106,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # 删除现有的子弹并新建一群外星人
+        # 删除现有的子弹,加快游戏节奏，并创建群新的外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
